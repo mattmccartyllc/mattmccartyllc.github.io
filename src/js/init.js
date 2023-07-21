2deb9981b4ed5adaa0ba9ea42454d438af572a58
Matt McCarty <mattmccarty@users.noreply.github.com>
2023-07-21 11:59:24 -0400

    Remove JS fix and add CSS
    Removing JS workaround and attempting to use dvh units in CSS.

--- src/js/init.js
+++ src/js/init.js
@@ -2,15 +2,6 @@ window.addEventListener("beforeunload", function(event) {
     window.scrollTo(0, 0);
 });
 
-// Fix viewport height (vh) unit issues for browsers that do not support dvh
-// https://stackoverflow.com/a/72245072
-// https://stackoverflow.com/a/43575432
-function vportHeight() {
-    document.body.style.height = window.innerHeight + "px";
-}
-window.addEventListener('resize', vportHeight);
-
 window.addEventListener('load', function () {
     sal();
-    vportHeight();
 });
