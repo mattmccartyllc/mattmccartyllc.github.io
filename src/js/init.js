a501f76ef9ccfdb5579bce44c968ea12a83e5d37
Matt McCarty <mattmccarty@users.noreply.github.com>
2023-07-21 11:52:04 -0400

    Js fix #5
    Add JS to fix viewport height issues on mobile.

--- src/js/init.js
+++ src/js/init.js
@@ -1,12 +1,16 @@
 window.addEventListener("beforeunload", function(event) {
     window.scrollTo(0, 0);
 });
-  
+
+// Fix viewport height (vh) unit issues for browsers that do not support dvh
+// https://stackoverflow.com/a/72245072
+// https://stackoverflow.com/a/43575432
+function vportHeight() {
+    document.body.style.height = window.innerHeight + "px";
+}
+window.addEventListener('resize', vportHeight);
+
 window.addEventListener('load', function () {
     sal();
-
-    // Fix viewport height (vh) unit issues for browsers that do not support dvh
-    // https://stackoverflow.com/a/72245072
-    // https://stackoverflow.com/a/43575432
-    document.body.style.height = window.innerHeight + "px";
+    vportHeight();
 });
